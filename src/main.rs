1b9e6597cade1df405be703d22369907b90a4156
Yuri <yuridokidokimnrx@gmail.com>
2025-06-22 10:55:27 +0200

    little changes 0.3v

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() {
         println!("Solved!\nx1 = {}", x);
     }
     if d < 0.0 {
-        println!("Solved!\n SQRTS not found!");
+        println!("Solved!\n SQRTS not found because {} < 0!", d);
     }
 
 }
