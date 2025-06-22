00f55be8f1156adcaeeb7ba7ad71175f71af53e0
Yuri <yuridokidokimnrx@gmail.com>
2025-06-22 11:02:36 +0200

    Adding loop and option for exit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io;
 fn main() {
     
-    println!("Program for solving quadratic equation");
+    
     
     let mut input_a: String = String::new();
     let mut input_b: String = String::new();
@@ -10,52 +10,63 @@ fn main() {
 
     // ax^2 + bx + c = 0
     // D = b^2 - 4(a*c)
-    println!("Enter a:");
-    match io::stdin().read_line(&mut input_a) {
-        Ok(_) => {},
-        Err(e) => println!("Failed to gather information. 
-        Error code: {}", e)
-    }
-    println!("Enter b:");
-    match io::stdin().read_line(&mut input_b) {
-        Ok(_) => {},
-        Err(e) => println!("Failed to gather information. 
-        Error code: {}", e)
-    }
-    println!("Enter c:");
-
-    match io::stdin().read_line(&mut input_c) {
-        Ok(_) => {},
-        Err(e) => println!("Failed to gather information. 
-        Error code: {}", e)
-    }
-    
-    //Convert String to float
-    let a: f64 = input_a.trim().parse().unwrap();
-    let b: f64 = input_b.trim().parse().unwrap();
-    let c: f64 = input_c.trim().parse().unwrap();
-    
-    //D-formula
-    let d:f64 = (b * b) - 4.0 * (a * c);
-    
-    println!("D = {}", d);
-
-
-    // Find √ 
-
-    if d > 0.0 {
-        let x1 = ((-b) + d.sqrt()) / (2.0 * a);
-        let x2 = ((-b) - d.sqrt()) / (2.0 * a);
-
-        println!("Solved!\nx1 = {}\nx2 = {}", x1, x2);
-    }
-    if d == 0.0 {
-        let x = (-b) / (2.0 * a);
+    loop {
+        println!("Program for solving quadratic equation");
         
-        println!("Solved!\nx1 = {}", x);
-    }
-    if d < 0.0 {
-        println!("Solved!\nSQRTS not found because D = {} < 0!", d);
-    }
+        println!("Type exit for exit!");
+        println!("Enter a:");
+        
+        match io::stdin().read_line(&mut input_a) {
+            Ok(_) => {},
+            Err(e) => println!("Failed to gather information. 
+            Error code: {}", e)
+        }
+        
+        println!("Enter b:");
+        
+        match io::stdin().read_line(&mut input_b) {
+            Ok(_) => {},
+            Err(e) => println!("Failed to gather information. 
+            Error code: {}", e)
+        }
+        
+        println!("Enter c:");
 
+        match io::stdin().read_line(&mut input_c) {
+            Ok(_) => {},
+            Err(e) => println!("Failed to gather information. 
+            Error code: {}", e)
+        }
+        if input_a == "exit" || input_b == "exit" || input_c == "exit" {
+            break;
+        }
+        //Convert String to float
+        let a: f64 = input_a.trim().parse().unwrap();
+        let b: f64 = input_b.trim().parse().unwrap();
+        let c: f64 = input_c.trim().parse().unwrap();
+        
+        //D-formula
+        let d:f64 = (b * b) - 4.0 * (a * c);
+        
+        println!("D = {}", d);
+
+        // Find √ 
+
+        if d > 0.0 {
+            
+            let x1 = ((-b) + d.sqrt()) / (2.0 * a);
+            
+            let x2 = ((-b) - d.sqrt()) / (2.0 * a);
+
+            println!("Solved!\nx1 = {}\nx2 = {}", x1, x2);
+        }
+        if d == 0.0 {
+            let x = (-b) / (2.0 * a);
+            
+            println!("Solved!\nx1 = {}", x);
+        }
+        if d < 0.0 {
+            println!("Solved!\nSQRTS not found because D = {} < 0!", d);
+        }
+    }
 }
